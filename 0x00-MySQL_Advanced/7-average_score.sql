82fb7268e5e2869d9bb174b12ae63d5dd7e2c564
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-05-09 09:04:28 +0100

    fast_push

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,25 +1,14 @@
 -- score
 DELIMITER //
 
-CREATE PROCEDURE ComputeAverageScoreForUser(
-    IN user_id INT
-)
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
-    DECLARE total_score FLOAT;
-    DECLARE total_projects INT;
-    SELECT SUM(score) INTO total_score
-    FROM corrections
-    WHERE user_id = user_id;
-    SELECT COUNT(DISTINCT project_id) INTO total_projects
-    FROM corrections
-    WHERE user_id = user_id;
-    IF total_projects > 0 THEN
-        SET total_score = total_score / total_projects;
-    END IF;
-    UPDATE users
-    SET average_score = total_score
-    WHERE id = user_id;
-END//
+	UPDATE users
+	SET
+	average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
+	WHERE id = user_id;
+
+END //
 
 DELIMITER ;
-
