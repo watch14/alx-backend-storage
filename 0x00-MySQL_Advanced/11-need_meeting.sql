936de24bc680cd6f53fd9c121c4c8cfe9c99293d
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-05-09 09:15:49 +0100

    fast_push

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,14 +1,5 @@
---  creates a function SafeDiv that divides (and returns) the first by the second number or returns 0 if the second number is equal to 0.
-
-DELIMITER //
-
-DROP FUNCTION IF EXISTS SafeDiv;
-CREATE FUNCTION SafeDiv (
-a INT,
-b INT)
-RETURNS FLOAT DETERMINISTIC
-BEGIN
-    RETURN (IF (b = 0, 0, a / b));
-END //
-
-DELIMITER;
+-- meetings
+CREATE VIEW need_meeting AS
+SELECT name
+FROM students
+WHERE (score < 80 OR last_meeting IS NULL OR last_meeting < DATE_SUB(NOW(), INTERVAL 1 MONTH));
