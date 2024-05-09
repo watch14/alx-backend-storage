c6482ecdc6849700d6e382a8269fee2a43aac6a1
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-05-09 09:18:21 +0100

    fast_push

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -7,25 +7,21 @@ BEGIN
     DECLARE total_weight INT;
     DECLARE avg_score FLOAT;
 
-    -- Initialize variables
     SET total_score = 0;
     SET total_weight = 0;
 
-    -- Calculate total weighted score
     SELECT SUM(corrections.score * projects.weight), SUM(projects.weight)
     INTO total_score, total_weight
     FROM corrections
     INNER JOIN projects ON corrections.project_id = projects.id
     WHERE corrections.user_id = user_id_param;
 
-    -- Compute average weighted score
     IF total_weight > 0 THEN
         SET avg_score = total_score / total_weight;
     ELSE
         SET avg_score = 0;
     END IF;
 
-    -- Update users table with the computed average score
     UPDATE users
     SET average_score = avg_score
     WHERE id = user_id_param;
