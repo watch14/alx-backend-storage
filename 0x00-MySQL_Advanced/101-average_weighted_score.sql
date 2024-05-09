ea585488a9de1f4dbbc6dbf9c4fdaafda080b652
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-05-09 09:22:05 +0100

    fast_push

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -3,48 +3,12 @@ DELIMITER //
 
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
-    DECLARE done INT DEFAULT FALSE;
-    DECLARE user_id_param INT;
-    DECLARE total_score FLOAT;
-    DECLARE total_weight INT;
-    DECLARE avg_score FLOAT;
-
-    DECLARE user_cursor CURSOR FOR
-        SELECT id FROM users;
-
-    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
-
-    OPEN user_cursor;
-
-    user_loop: LOOP
-        FETCH user_cursor INTO user_id_param;
-
-        IF done THEN
-            LEAVE user_loop;
-        END IF;
-
-        SET total_score = 0;
-        SET total_weight = 0;
-
-        SELECT SUM(corrections.score * projects.weight), SUM(projects.weight)
-        INTO total_score, total_weight
-        FROM corrections
-        INNER JOIN projects ON corrections.project_id = projects.id
-        WHERE corrections.user_id = user_id_param;
-
-        IF total_weight > 0 THEN
-            SET avg_score = total_score / total_weight;
-        ELSE
-            SET avg_score = 0;
-        END IF;
-
-        UPDATE users
-        SET average_score = avg_score
-        WHERE id = user_id_param;
-    END LOOP;
-
-    CLOSE user_cursor;
-
+	 UPDATE users set average_score = (SELECT
+    SUM(corrections.score * projects.weight) / SUM(projects.weight)
+    FROM corrections
+    INNER JOIN projects
+    ON projects.id = corrections.project_id
+    where corrections.user_id = users.id);
 END//
 
 DELIMITER ;
