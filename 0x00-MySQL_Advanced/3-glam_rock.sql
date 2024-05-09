1407a81a2f1852f01d7c7f5727d043e4b4ed888a
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-05-09 08:53:27 +0100

    fast_push

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,5 @@
 -- bands
-SELECT band_name,
-	IFNULL(split, 2022) - formed AS lifespan
+SELECT band_name, IFNULL(split, 2022) - formed AS lifespan
 FROM metal_bands
 WHERE main_style = 'Glam rock'
 ORDER BY lifespan DESC;
