166ec7c48e69e5712abb79ea6d606dbd4ad82e7c
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-05-09 09:10:00 +0100

    fast_push

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,14 @@
+-- div
+DELIMITER //
+
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT
+BEGIN
+    IF b = 0 THEN
+        RETURN 0;
+    ELSE
+        RETURN a / b;
+    END IF;
+END//
+
+DELIMITER ;
