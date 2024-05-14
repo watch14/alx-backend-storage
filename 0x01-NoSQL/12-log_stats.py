4d067fb347e073c703533abe2e97d5990ce480e3
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-05-14 17:09:31 +0100

    fast_push

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python3
-""" get from nginx """
+""" pymongo """
 from pymongo import MongoClient
 
 if __name__ == "__main__":
-    """ nginx info """
+    """ Nginx logs MongoDB """
     client = MongoClient('mongodb://127.0.0.1:27017')
-    collection = client.logs.nginx
+    nginx_collection = client.logs.nginx
 
-    print(f"{collection.count_documents({})} logs")
-    print("Methods:")
+    n_logs = nginx_collection.count_documents({})
+    print(f'{n_logs} logs')
 
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
+    print('Methods:')
     for method in methods:
-        count = collection.count_documents({'method': method})
-        print(f"    method {method}: {count}")
+        count = nginx_collection.count_documents({"method": method})
+        print(f'\tmethod {method}: {count}')
 
-    stat = collection.count_documents({'method': 'GET', 'path': '/status'})
-    print(f"{stat} status check")
+    status_check = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
+
+    print(f'{status_check} status check')
