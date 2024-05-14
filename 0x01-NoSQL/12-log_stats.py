c24bcdd66c23345c8f0aa33da9a286d4c21caaee
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-05-14 17:01:58 +0100

    fast_push

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -8,12 +8,13 @@ if __name__ == "__main__":
     collection = client.logs.nginx
 
     print(f"{collection.count_documents({})} logs")
-
+    print("Methods:")
+    
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
+    
     for method in methods:
         count = collection.count_documents({'method': method})
-        print(f"method {method}: {count}")
+        print(f"    method {method}: {count}")
 
     stat = collection.count_documents({'method': 'GET', 'path': '/status'})
     print(f"{stat} status check")
