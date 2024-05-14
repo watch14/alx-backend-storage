d46db661752e5a42def09620bea4181f850b9e75
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-05-14 17:16:32 +0100

    fast_push

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """ get from nginx """
 from pymongo import MongoClient
-from collections import Counter
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
@@ -19,8 +18,15 @@ if __name__ == "__main__":
     stat = collection.count_documents({'method': 'GET', 'path': '/status'})
     print(f"{stat} status check")
 
-    print("IPs:")
-    ips = collection.distinct("ip")
-    ip_counts = Counter(ips).most_common(10)
-    for ip, count in ip_counts:
-        print(f"\t{ip}: {count}")
+    pipeline = [
+            {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+            {"$sort": {"count": -1}},
+            {"$limit": 10}
+            ]
+
+    top_ips = collection.aggregate(pipeline)
+
+    for index, ip_data in enumerate(top_ips, 1):
+        ip = ip_data['_id']
+        count = ip_data['count']
+        print(f"\t{index}. {ip}: {count}")
