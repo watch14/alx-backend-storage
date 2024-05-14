2125cde5a6fdda8592825f93c8cfe45d7204bb1d
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-05-14 14:11:28 +0100

    fast_push

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" update """
+from pymongo import MongoClient
+
+
+def update_topics(mongo_collection, name, topics):
+    """ update topics """
+    mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
