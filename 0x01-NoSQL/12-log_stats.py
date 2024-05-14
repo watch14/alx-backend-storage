dd86cc5bb65e10d314dbfcb629f2727528f71804
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-05-14 16:59:02 +0100

    fast_push

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,4 +5,15 @@ from pymongo import MongoClient
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
-    school_collection = client.my_db.school
+    collection = client.logs.nginx
+
+    print(f"{collection.count_documents({})} logs")
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+    for method in methods:
+        count = collection.count_documents({'method': method})
+        print(f"method {method}: {count}")
+
+    stat = collection.count_documents({'method': 'GET', 'path': '/status'})
+    print(f"{stat} status check")
