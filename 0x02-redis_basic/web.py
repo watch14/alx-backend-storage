6681d0ef38307482c21c9eb56f7d531b29d48788
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-05-15 13:34:38 +0100

    fast_push

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,25 +2,37 @@
 """ web """
 import requests
 import redis
+from functools import wraps
 
 
+def track_and_cache(func):
+    """ track / cache """
+    @wraps(func)
+    def wrapper(url):
+        """ rap god """
+        redis_conn = redis.Redis()
+
+        url_count_key = f"count:{url}"
+        redis_conn.incr(url_count_key)
+
+        cached_content = redis_conn.get(url)
+        if cached_content:
+            return cached_content.decode('utf-8')
+
+        response = requests.get(url)
+        page_content = response.text
+
+        redis_conn.setex(url, 10, page_content)
+
+        return page_content
+
+    return wrapper
+
+
+@track_and_cache
 def get_page(url: str) -> str:
     """ page """
-    redis_conn = redis.Redis()
-
-    url_count_key = f"count:{url}"
-    redis_conn.incr(url_count_key)
-
-    cached_content = redis_conn.get(url)
-    if cached_content:
-        return cached_content.decode('utf-8')
-
-    response = requests.get(url)
-    page_content = response.text
-
-    redis_conn.setex(url, 10, page_content)
-
-    return page_content
+    return requests.get(url).text
 
 
 if __name__ == "__main__":
