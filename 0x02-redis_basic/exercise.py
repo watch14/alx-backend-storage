3296229ee656704cf55a2689609066e5fb8ceab0
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-05-15 12:15:35 +0100

    fast_push

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -9,13 +9,13 @@ from typing import Union
 
 class Cache():
     """ cache class """
-    def __init__(self, redis):
+    def __init__(self):
         """ init """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(data: Union[str, bytes, int, float]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """store date """
         key = str(uuid.uuid4())
-        self_redis.set(key, data)
+        self._redis.set(key, data)
         return key
